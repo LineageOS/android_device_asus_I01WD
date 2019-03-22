860d7ce6b7126eae243f274bb7cb63e299351a43
Vladimir Oltean <olteanv@gmail.com>
2019-03-22 22:37:05 +0000

    I01WD: Sync extractors with templates
     * This introduces fixup scripts and kang mode and fixes
       passing of arguments longer than one word,
       such as --section "Hello world".
    
    Change-Id: I42dde659e9bea4f7e1c9a7a3fa8da3243774ca4e

--- extract-files.sh
+++ extract-files.sh
@@ -22,46 +22,60 @@ VENDOR=asus
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="${MY_DIR}"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
+SECTION=
+KANG=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
 
 # Fix proprietary blobs
-patchelf --remove-needed android.hidl.base@1.0.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib/libfm-hci.so
-patchelf --remove-needed android.hidl.base@1.0.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib64/libfm-hci.so
-patchelf --remove-needed android.hidl.base@1.0.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib64/liblocationservice_jni.so
-patchelf --remove-needed android.hidl.base@1.0.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib64/libxt_native.so
+patchelf --remove-needed android.hidl.base@1.0.so \
+        "${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary/lib/libfm-hci.so"
+patchelf --remove-needed android.hidl.base@1.0.so \
+        "${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary/lib64/libfm-hci.so"
+patchelf --remove-needed android.hidl.base@1.0.so \
+        "${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary/lib64/liblocationservice_jni.so"
+patchelf --remove-needed android.hidl.base@1.0.so \
+        "${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary/lib64/libxt_native.so"
 
-sed -i 's/<library name="android.hidl.manager-V1.0-java"/<library name="android.hidl.manager@1.0-java"/g' "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/etc/permissions/qti_libpermissions.xml
+sed -i 's/<library name="android.hidl.manager-V1.0-java"/<library name="android.hidl.manager@1.0-java"/g' \
+        "${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary/etc/permissions/qti_libpermissions.xml"
 
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
