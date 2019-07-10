8ac88cf566c5d1b157b08c22e6a224e0a9891ce5
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-10 20:42:18 +0200

    I01WD: Add support to ASUS motor
    Change-Id: Ia38eddb6729d4a111d6f4a89cd832dbe39393947

--- camera_motor/service.cpp
+++ camera_motor/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.camera.motor@1.0-service.asus_msmnile"
+#define LOG_TAG "lineage.camera.motor@1.0-service.asus_msmnile"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
