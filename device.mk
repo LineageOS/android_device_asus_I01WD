01746a96d76fadf5188e67e40e8e8fee1f51f978
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-14 18:57:17 +0200

    I01WD: Build common Trust HAL
    Change-Id: I73a24535534a696ae42cdda3195c60aa2e50ff33

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.touch@1.0-service.asus_msmnile
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
+
 # Update engine
 PRODUCT_PACKAGES += \
     brillo_update_payload \
