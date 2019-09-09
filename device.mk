7e7398c266710c5dca4f0ea81e9ab3f61b183132
LuK1337 <priv.luk@gmail.com>
2019-09-09 20:50:55 +0200

    I01WD: Move PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE to common.mk
    Change-Id: I2751014af71f222bd43e4bc17fc75d20499ea2f1

--- device.mk
+++ device.mk
@@ -34,6 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
