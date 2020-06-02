446a7cb163321d5256a01560382fc65b06947bc9
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-06-02 17:53:52 +0200

    I01WD: Enable LMKD
    Change-Id: Ib8c7e36b38e1649e01d0f69788e19cc295efc542

--- system_prop.mk
+++ system_prop.mk
@@ -74,6 +74,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.overlay.izat.optin=rro
 
+# LMKD
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.low_ram=false \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true \
+    ro.lmk.log_stats=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/system/etc/media_profiles_vendor.xml
